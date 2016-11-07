412aba4c195744039d564e0fe5b7eb4d7258ad45
sr100 <SalvadorRivera@Energex.com.au>
2016-11-07 16:13:31 +1000

    added correct service variables names

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var generateMongoUrl = function(){
 	  if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD //OpenShift V2 
 		|| process.env.MONGODB_PASSWORD){ // OpenShift V3
 	    connection_string = (process.env.OPENSHIFT_MONGODB_DB_USERNAME||process.env.MONGODB_USER) + ":" +
-	    (process.env.OPENSHIFT_MONGODB_DB_PASSWORD||process.env.MONGDB_PASSWORD) + "@" +
+	    (process.env.OPENSHIFT_MONGODB_DB_PASSWORD||process.env.MONGODB_PASSWORD) + "@" +
 	    (process.env.OPENSHIFT_MONGODB_DB_HOST||process.env.MONGODB_SERVICE_HOST) + ':' +
 	    (process.env.OPENSHIFT_MONGODB_DB_PORT||process.env.MONGODB_SERVICE_PORT) + '/' +
 	    (process.env.OPENSHIFT_APP_NAME||process.env.MONGODB_DATABASE);
